ab20d64d3c0fd09350f50fe4475d3975ae14057b
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-05-16 13:07:45 +0200

    footer da finire

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -1,11 +1,14 @@
 <footer class="footer">
 
     <div class="footer__info">
-        <?= $this->element('snippet', ['id' => 1])?>
-    </div>
-    
-    <div class="footer__menu">
-        <?= $this->cell('Menu.Menu', [8]) ?>
+        <div class="footer__info__agency">
+            <label>azienda</label>
+            <?= $this->element('snippet', ['id' => 1])?>
+        </div>
+        <div>
+            <label>contatti</label>
+            <?= $this->element('snippet', ['id' => 15])?>
+        </div>
     </div>
 
     <div class="footer__legal">
@@ -14,13 +17,13 @@
             <li><?= $this->Frontend->seolink(__d('policies', 'Cookie policy'), '/policies/view/2'); ?></li>
             <li><span id="cookie_reload"><?php echo __d('policies', 'manage cookies'); ?></span></li>
         </ul>
-        
     </div>
 
-    <div class="footer__social">
-        <?= $this->cell('Menu.Menu', [7]) ?>
+    <div class="footer__bottom">
+        <?= $this->element('snippet', ['id' => 16])?>
     </div>
-    <div class="footer__credits">
+
+    <!-- <div class="footer__credits">
         <a href="https://www.webmotion.it" title="Web Agency Verona">Credits</a>
-    </div>
+    </div> -->
 </footer>
