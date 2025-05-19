5165e58b23a4395c386d4cdf93f9f2ec424bc724
Salvatore <>
2025-05-19 14:41:11 +0200

    lavorando sulla pagina prodotti (bug sugli elementi)

--- plugins/Products/templates/Products/view.php
+++ plugins/Products/templates/Products/view.php
@@ -1,24 +1,46 @@
-
-<article class="product">
-
-    <?php if (!empty($images['preview'])): ?>
-        <div class="product__preview">
-            <img src="<?= $this->Frontend->resize($images['preview']->path, 1920) ?>" alt="<?= h($images['preview']->title) ?>">
-        </div>
-    <?php endif;  ?>
-
-    <div class="product__description">
-        <h1><?= $item->title ?></h1>
-
-        <?php echo $item->text ?>
+<article class="product pt-h">
+    <div class="text-center product__title font-64">
+        <h1 class="uppercase"><?= $item->title ?></h1>
+        <div class="font-secondary fw-light font-20"><?= $item->subname ?? ''?></div>
     </div>
-
-
-    <?php if (!empty($images['gallery'])): ?>
-        <div class="product__gallery">
-            <?php echo $this->element('gallery', ['images' => $images['gallery']]) ?>
+    <?php if (!empty($images['preview'])): ?>
+        <div class="product__preview m-auto mb-251">
+            <img src="<?= $this->Frontend->image($images['preview']->path) ?>" alt="<?= h($images['preview']->title) ?>">
         </div>
     <?php endif; ?>
 
+    <div class="product__description font-48 mb-307 text-center m-auto container-paragraph">
+        <?php echo $item->description ?>
+    </div>
 
+    <?php 
+        $elements = [
+            [
+                'name' => $elemento_1,
+                'text' => $info_element_1
+            ],
+            [
+                'name' => $elemento_2,
+                'text' => $info_element_2
+            ],
+            [
+                'name' => $elemento_3,
+                'text' => $info_element_3
+            ],
+        ]
+    ?>
+
+    <?php if (!empty($images['gallery_2'])): ?>
+        <div class="product__gallery container-m m-auto mb-224">
+            <?php echo $this->element('gallery', [
+                'images' => $images['gallery_2'],
+                'elements' => $elements
+            ]) ?>
+        </div>
+    <?php endif; ?>
+
+    <?= $this->element('triple-img', [
+        'images' => $images['gallery_1'],
+        'extraClass' => 'triple-img--products'
+    ]) ?>
 </article>
--- templates/element/gallery.php
+++ templates/element/gallery.php
@@ -1,9 +1,12 @@
-<?php if (!empty($images)): ?>
+<?php if (!empty($images) && count($images) === count($elements)): ?>
     <div class="gallery">
-        <?php foreach ($images as $img): ?>
-            <a href="<?= $this->Frontend->resize($img->path, 1920) ?>" data-fancybox="gallery">
-                <img src="<?= $this->Frontend->crop($img->path, 600, 400) ?>" alt="<?= $img->title ?>">
-            </a>
+        <?php foreach (array_values($images) as $i => $img): ?>
+            <?php $element = $elements[$i]; ?>
+            <div data-fancybox="gallery" class="gallery__img">
+                <img src="<?= $this->Frontend->image($img->path) ?>" alt="<?= $img->title ?>">
+                <h1><?= $element['name'] ?? '' ?></h1>
+                <p><?= $element['text'] ?? '' ?></p>
+            </div>
         <?php endforeach; ?>
     </div>
 <?php endif; ?>
