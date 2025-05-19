94b48d3004ce3ca330f8296f8f3d95ac3221975a
Salvatore <>
2025-05-19 17:06:51 +0200

    creata animazione di entrata menu

--- templates/element/mobile-menu.php
+++ templates/element/mobile-menu.php
@@ -1,4 +1,3 @@
 <div class="mobile-menu" data-mobile-menu>
-    <span class="mobile-menu__close" data-menu-toggler></span>
     <?= $this->cell('Menu.Menu', [1, ['showBackArrow' => true]]) ?>
 </div>
